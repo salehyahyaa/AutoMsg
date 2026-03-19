744fdb49559cfa6b28f743118679446e079ecfda
Saleh Yahya <salehyahya10.20@gmail.com>
2026-03-18 22:45:15 -0500

    testing

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from messageSender import MessageSender
 from contacts import contacts
 from src.messages import msgs 
 
+iMsgSender = MessageSender()                            #we instaintate a class as setup when we want to start instainizating objects from it
 
-def send_messege(messges): 
+
+def send_messege(event): 
     ...
